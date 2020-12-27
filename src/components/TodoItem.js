86959ac39b1334e6ad0a6dabd863e62468d9c57e
irini <cyishere@gmail.com>
2020-12-27 18:16:34 +0800

    Update in own component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,44 @@
-const TodoItem = ({
-  todo,
-  deleteTodo,
-  toggleCompleted,
-  handleChange,
-  updateTodo,
-}) => {
+import { useState } from "react";
+import { updateTodoService } from "../services/todoServices";
+
+const TodoItem = ({ todo, deleteTodo, toggleCompleted }) => {
   const classes = todo.completed ? "completed" : "";
+  const [update, setUpdate] = useState(todo.content);
+  const [status, setStatus] = useState("idle");
+
+  const handleCancel = () => {
+    setStatus("edle");
+  };
+
+  const handleUpdateChange = (e) => {
+    setStatus("editing");
+    setUpdate(e.target.value);
+  };
+
+  const updateTodo = async (todoId, origin) => {
+    setStatus("idle");
+    const updateInfo = { ...origin, content: update };
+    await updateTodoService(todoId, updateInfo);
+  };
+
+  const btnControls =
+    status === "editing" ? (
+      <>
+        <button
+          className="btn secondary"
+          onClick={() => updateTodo(todo.id, todo)}
+        >
+          Save
+        </button>
+        <button className="btn" onClick={handleCancel}>
+          Cancel
+        </button>
+      </>
+    ) : (
+      <button className="btn text" onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
+    );
 
   return (
     <li className={`todo-item ${classes}`}>
@@ -20,20 +53,13 @@ const TodoItem = ({
         <input
           type="text"
           className="todo-item_content"
-          value={todo.content}
-          onChange={handleChange}
+          value={update}
+          onChange={handleUpdateChange}
           disabled={todo.completed}
         />
       </div>
 
-      <div className="todo-item_control">
-        <button className="btn secondary" onClick={updateTodo}>
-          Edit
-        </button>
-        <button className="btn text" onClick={() => deleteTodo(todo.id)}>
-          Delete
-        </button>
-      </div>
+      <div className="todo-item_control">{btnControls}</div>
     </li>
   );
 };
